612959d281e107e0430068d76487a06c075c146f
touhidx <touhidul0147@gmail.com>
2026-09-12 23:31:41 +0600

    added optional rendering for your stack section ui

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -17,18 +17,32 @@ const SideBar = ({ stack, setStack }: Iprop) => {
       <h3 className="text-2xl font-bold  mb-1.5 text-(--color-app-navy) ">
         Your Stack
       </h3>
-      <p className="text-sm text-(--color-app-navy) leading-relaxed mb-4 ">
-        {" "}
-        {stack.length} Technology Selected
-      </p>
-      <StackItem stack={stack} setStack={setStack}></StackItem>
 
-      <button
-        onClick={() => removeAll()}
-        className="w-full mt-3 py-2.5 rounded-lg border border-red-200 text-red-500 hover:bg-red-50 text-sm font-medium"
-      >
-        Remove All
-      </button>
+      <p className="text-sm text-(--color-app-navy) leading-relaxed mb-4 ">
+        {stack.length !== 0
+          ? `${stack.length} Technology Selected`
+          : "No Technology Selected"}
+      </p>
+      {stack.length !== 0 ? (
+        <>
+          <StackItem stack={stack} setStack={setStack}></StackItem>
+
+          <button
+            onClick={() => removeAll()}
+            className="w-full mt-3 py-2.5 rounded-lg border border-red-200 text-red-500 hover:bg-red-50 text-sm font-medium"
+          >
+            Remove All
+          </button>
+        </>
+      ) : (
+        <>
+          <div className="px-8 py-7 border border-dotted rounded-2xl">
+            <p className="text-gray-400 text-lg text-center">
+              Your stack is empty
+            </p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
