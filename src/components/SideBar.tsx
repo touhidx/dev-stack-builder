1f714e29c21aa529832f70549f0cdb87d10021d1
touhidx <touhidul0147@gmail.com>
2026-09-12 23:12:01 +0600

    added functionality of remove all btn

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -8,16 +8,25 @@ interface Iprop {
   setStack: Dispatch<SetStateAction<Iprops[]>>;
 }
 const SideBar = ({ stack, setStack }: Iprop) => {
+  const removeAll = () => {
+    setStack([]);
+  };
   console.log(stack);
   return (
-    <div className=" border-(--color-app-surface) border bg-(--color-app-bg) rounded-2xl p-5 flex flex-col">
+    <div className=" border-(--color-app-surface) border bg-(--color-app-bg) rounded-2xl p-5 ">
       <h3 className="text-2xl font-bold  mb-1.5 text-(--color-app-navy) ">
         Your Stack
       </h3>
-      <p className="text-sm text-(--color-app-navy) leading-relaxed mb-4 flex-1"></p>
+      <p className="text-sm text-(--color-app-navy) leading-relaxed mb-4 ">
+        {" "}
+        {stack.length} Technology Selected
+      </p>
       <StackItem stack={stack} setStack={setStack}></StackItem>
 
-      <button className="w-full mt-3 py-2.5 rounded-lg border border-red-200 text-red-500 hover:bg-red-50 text-sm font-medium">
+      <button
+        onClick={() => removeAll()}
+        className="w-full mt-3 py-2.5 rounded-lg border border-red-200 text-red-500 hover:bg-red-50 text-sm font-medium"
+      >
         Remove All
       </button>
     </div>
